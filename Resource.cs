a0e562e1abf0eb0bb38aa177297847c6012c8656
FixedF0x <85841428+FixedF0x@users.noreply.github.com>
2021-06-13 18:48:04 +0200

    Initial commit

---
+++ Resource.cs
@@ -0,0 +1,19 @@
+﻿using AltV.Net.Async;
+using StoryLineRP.Events;
+using System;
+
+namespace StoryLineRP
+{
+    public class Resource : AsyncResource
+    {
+        public override void OnStart()
+        {
+            AltAsync.Log("StoryLine-RP Test loaded.");
+        }
+
+        public override void OnStop()
+        {
+            AltAsync.Log("StoryLine-RP Test unloaded.");
+        }
+    }
+}
